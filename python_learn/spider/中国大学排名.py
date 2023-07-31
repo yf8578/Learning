0e94ac4df3010d4506ad7ebefa8fc37f2acd3636
zhangyifan1 <zhangyifan1@genomics.cn>
2023-07-31 11:40:51 +0800

    中国大学排名

--- python_learn/spider/中国大学排名.py
+++ python_learn/spider/中国大学排名.py
@@ -1,13 +1,13 @@
-# -*-encoding:utf-8-*-
 """
 Author: zhangyifan1
 Date: 2023-07-30 18:46:41
 LastEditors: zhangyifan1 zhangyifan1@genomics.cn
-LastEditTime: 2023-07-31 09:10:22
+LastEditTime: 2023-07-31 11:39:00
 FilePath: //spider//中国大学排名.py
 Description: 
 
 """
+# -*-encoding:utf-8-*-
 import requests
 from bs4 import BeautifulSoup
 import bs4
@@ -26,22 +26,28 @@ def getHTMLText(url):  # 访问网址
 
 def fillUnivList(ulist, html):  # 获取关键信息，添加到列表中
     soup = BeautifulSoup(html, "html.parser")
+    # text = soup.tbody.tr.td.p
+    # print(text)
     for tr in soup.find("tbody").children:
-        if isinstance(tr, bs4.element.Tag):  # 因为标签中会存在字符串，所以需要过滤掉非标签类型的其他信息
+        # print(tr)
+        #     print("next line")
+        # print(tr)
+        if isinstance(tr, bs4.element.Tag):  # 因为标签中会存在字符串，需要过滤掉非标签类型的其他信息
             td = tr("td")
-            print(td.string)
-            # print(type(td))
-        #     tds = tr("td")  # 对tr标签中的td标签进行筛选
-        #     ulist.append([tds[0].string,tds[1].string,tds[2].string])
+            # print(td[1].p.string)
+            # print(t)
+            ulist.append(
+                [td[0].p.string, td[1].p.string, td[2].p.string, td[3].p.string]
+            )
 
 
-# def printUnivList(ulist, num):
-#     print("{:^10}\t{:^6}\t{:^10}".format("排名", "学校名称", "总分"))
-#     for i in range(num):
-#         u = ulist[i]
-#         print(u)
-#     print("{:^10}\t{:^6}\t{:^10}".format(u[0], u[1], u[2]))
-#     print("Suc" + str(num))
+def printUnivList(ulist, num):
+    # print("{:^10}\t{:^6}\t{:^10}".format("排名", "学校名称", "总分"))
+    for i in range(num):
+        u = ulist[i]
+        # print(u)
+        print("{:^10}\t{:^6}\t{:^10}\t{:^6}".format(u[0], u[1], u[2], u[3]))
+    print("Suc" + str(num - 1))
 
 
 def main():
@@ -51,7 +57,8 @@ def main():
     print("step1")
     # print(html)
     fillUnivList(unifo, html)
-    # printUnivList(unifo, 20)  # get 20 universities
+    printUnivList(unifo, 21)  # get 20 universities
 
 
-main()
+if __name__ == "__main__":
+    main()
